c5ab939fae8f6a38f186a924ac83f0a23216da66
Ben Marvell <madmarv187@gmail.com>
2014-08-08 16:51:19 +0100

    Updated javascript to proper commonjs style.
    Fixed all the errors that will get thrown from ```npm run-script lint```

--- test/emailer.spec.js
+++ test/emailer.spec.js
@@ -1,16 +1,15 @@
-var expect = require("chai").expect;
-var Emailer = require("../lib/emailer.js");
+var expect = require('chai').expect,
+  Emailer = require('../lib/emailer');
 
-describe("Emailer", function(){
-    describe("#sendEmail()", function(){
-        it("should send a hello world to nowhere", function(){
-            var emailer = new Emailer({
-                message: "hello world"
-            });
-            var results = emailer.sendEmail();
+describe('Emailer', function () {
+  describe('#sendEmail()', function () {
+    it('should send a hello world to nowhere', function () {
+      var emailer = new Emailer({
+        message: 'hello world'
+      }),
+      results = emailer.sendEmail();
 
-            expect(results).to.equal("hello world");
-
-        });
+      expect(results).to.equal('hello world');
     });
+  });
 });
