b84027307e542c4f9c8e3978a3f1290db94834b0
Ben Marvell <madmarv187@gmail.com>
2014-08-13 15:56:07 +0100

    Add sinon and sinon-chai
    Test the emailer properly

--- test/emailer.spec.js
+++ test/emailer.spec.js
@@ -1,19 +1,48 @@
-var expect = require('chai').expect,
-    Emailer = require('../lib/emailer');
+var Emailer = require('../lib/emailer'),
+    config = require('../config.json');
 
 describe('Emailer', function () {
-  var email;
+  var email,
+      nodemailer = require('nodemailer');
 
   beforeEach(function () {
+    sinon.spy(nodemailer, 'createTransport');
+
     email = new Emailer();
+
+    sinon.stub(email.transporter, 'sendMail');
   });
 
-  describe('.sendEmail()', function () {
-    it('should send a hello world to nowhere', function () {
-      expect(email).to.be.Object;
+  afterEach(function () {
+    email.transporter.sendMail.restore();
+    nodemailer.createTransport.restore();
+  });
+
+  describe('instantiation', function () {
+    it('should create a transport with the correct key and secret', function () {
+      config.amazonAWS.accessKeyId = 'AMAZONKEY';
+      config.amazonAWS.secretAccessKey = 'AMAZONSECRET';
+
+      /* jshint unused: false */
+      var mail = new Emailer(),
+          nodeMailerOptions = nodemailer.createTransport.getCall(1).args[0].options;
+
+      nodeMailerOptions.accessKeyId.should.equal('AMAZONKEY');
+      nodeMailerOptions.secretAccessKey.should.equal('AMAZONSECRET');
     });
   });
 
+  describe('.sendEmail()', function () {
+    it('should call sendMail on the transport with the correct args', function () {
+      email.sendEmail('test@test.com', 'subject', 'body');
 
+      email.transporter.sendMail.should.have.been.calledWith({
+        from: config.notificationsEmail,
+        to: 'test@test.com',
+        subject: 'subject',
+        text: 'body'
+      });
+    });
+  });
 
 });
