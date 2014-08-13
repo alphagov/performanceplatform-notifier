f8acdab2fbc1c83f20359822f4c0c67dcf9ba368
Ben Marvell <madmarv187@gmail.com>
2014-08-13 12:18:44 +0100

    Added nonsense test.

--- test/emailer.spec.js
+++ test/emailer.spec.js
@@ -1,15 +1,19 @@
 var expect = require('chai').expect,
-  Emailer = require('../lib/emailer');
+    Emailer = require('../lib/emailer');
 
 describe('Emailer', function () {
-  describe('#sendEmail()', function () {
-    it('should send a hello world to nowhere', function () {
-      var emailer = new Emailer({
-        message: 'hello world'
-      }),
-      results = emailer.sendEmail();
+  var email;
 
-      expect(results).to.equal('hello world');
+  beforeEach(function () {
+    email = new Emailer();
+  });
+
+  describe('.sendEmail()', function () {
+    it('should send a hello world to nowhere', function () {
+      expect(email).to.be.Object;
     });
   });
+
+
+
 });
