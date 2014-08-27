c5fe083686ff372c9b61e384d225b98afc3d1559
Ben Marvell <madmarv187@gmail.com>
2014-08-27 12:16:00 +0100

    Added two new backdrop endpoints.
    emailSent POSTS data/performance-platform/notifier with the dataset and timestamp
    
    lastEmailSent GETS with a QS for the last time a data_set was sent an email
    
    Tests and stuff

--- test/integrations/backdrop.spec.js
+++ test/integrations/backdrop.spec.js
@@ -9,26 +9,34 @@ describe('Backdrop integration', function () {
   beforeEach(function () {
     deferred = Q.defer();
     sinon.stub(Query.prototype, 'get').returns(deferred.promise);
+    sinon.stub(Query.prototype, 'post').returns(deferred.promise);
     Backdrop = require('../../lib/integrations/backdrop');
   });
 
   afterEach(function () {
     Query.prototype.get.restore();
+    Query.prototype.post.restore();
   });
 
   it('should accept options', function () {
     var backdrop = new Backdrop({
-      foo: 'bar'
+      foo: 'bar',
+      auth: {
+        bearer: ''
+      }
     });
 
     backdrop.config.should.eql({
-      baseUrl : 'https://www.performance.service.gov.uk',
+      baseUrl : 'https://www.performance.service.gov.uk/',
+      auth: {
+        bearer: ''
+      },
       foo: 'bar'
     });
 
   });
 
-  describe('Querying for out of date datasets', function () {
+  describe('getDataSets', function () {
 
     it('Should respond correctly to datasets', function () {
 
@@ -41,7 +49,7 @@ describe('Backdrop integration', function () {
 
           Query.prototype.get.should.be.calledOnce;
           Query.prototype.get.getCall(0).args[0]
-            .should.equal('/_status/data-sets/');
+            .should.equal('_status/data-sets/');
 
           response.should.be.an.instanceOf(Object);
           response.should.have.property('data_sets').
@@ -78,6 +86,69 @@ describe('Backdrop integration', function () {
         });
     });
 
+  });
+
+  describe('emailSent', function () {
+    beforeEach(function () {
+      this.clock = sinon.useFakeTimers(
+        new Date(2014, 7, 20).getTime()
+      );
+    });
+
+    it('should call the client.post endpoint with the dataset and a timestamp', function () {
+      var backdrop = new Backdrop({
+        auth: {
+          bearer: ''
+        }
+      });
+
+      deferred.resolve();
+
+      return backdrop.emailSent('test_data_set').then(function () {
+        Query.prototype.post.should.be.calledOnce;
+        Query.prototype.post.getCall(0).args[0]
+            .should.equal('data/performance-platform/notifier');
+        Query.prototype.post.getCall(0).args[1]
+            .should.eql({
+              auth: {
+                bearer: ''
+              },
+              baseUrl: 'https://www.performance.service.gov.uk/',
+              json: {
+                _timestamp: '2014-08-19T23:00:00+00:00',
+                data_set: 'test_data_set'
+              }
+            });
+      });
+    });
+  });
+
+  describe('lastEmailSent', function () {
+    beforeEach(function () {
+      this.clock = sinon.useFakeTimers(
+        new Date(2014, 7, 20).getTime()
+      );
+    });
+
+    it('should call client.get with the correct dataset (filter) and options', function () {
+      var backdrop = new Backdrop();
+
+      deferred.resolve();
+
+      return backdrop.lastEmailSent('test_data_set').then(function () {
+        Query.prototype.get.should.be.calledOnce;
+        Query.prototype.get.getCall(0).args[0]
+            .should.equal('data/performance-platform/notifier');
+        Query.prototype.get.getCall(0).args[1]
+            .should.eql({
+              qs: {
+                filter_by: 'data_set:test_data_set',
+                limit: 1,
+                sort_by: '_timestamp:descending'
+              }
+            });
+      });
+    });
   });
 
 });
