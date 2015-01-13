00424469c8ff62af11eb2f23dba288f013c141d3
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2015-01-13 14:10:32 +0000

    Bump client version and use new client API
    To take advantage of refactored client & views. Grouped time series and realtime modules not supported yet.

--- test/process-modules.spec.js
+++ test/process-modules.spec.js
@@ -3,43 +3,50 @@ describe('Process summary modules', function () {
 
   beforeEach(function () {
     this.dashboardConfig = require('../node_modules/performanceplatform-client.js' +
-    '/test/fixtures/dashboard-processed.json');
+    '/test/fixtures/dashboard-response.json');
+    this.dashboardConfig.modules = [];
+    this.dashboardConfig.modules.push(require('../node_modules/performanceplatform-client.js' +
+    '/test/fixtures/module-config-kpi.json'));
+    this.dashboardConfig.modules.push(require('../node_modules/performanceplatform-client.js' +
+    '/test/fixtures/module-config-single-time-series.json'));
+    this.dashboardConfig.modules.push(require('../node_modules/performanceplatform-client.js' +
+    '/test/fixtures/module-config-user-satisfaction-graph.json'));
     this.processed = processModules(this.dashboardConfig.modules);
   });
 
   it('returns an array of modules', function () {
-    this.processed.length.should.equal(5);
+    this.processed.length.should.equal(3);
   });
 
   it('should return modules with title property', function () {
     var module = this.processed[0];
-    module.title.should.equal('Transactions per year');
+    module.moduleConfig.title.should.equal('test');
   });
 
   it('should return a textUpdate for a KPI module', function () {
     var module = this.processed[0];
     module.textUpdate.should.eql([
-      'July 2013 to June 2014 = 45.8m',
-      'Apr 2013 to Mar 2014 = 46m',
-      'Total change = -0.27%'
+      '1 July 2013 to 30 June 2014 = 1',
+      '1 April 2013 to 30 June 2014 = 2',
+      'Total change = −50.00%'
     ]);
   });
 
   it('should return a textUpdate for a single time series module', function () {
-    var module = this.processed[3];
+    var module = this.processed[1];
     module.textUpdate.should.eql([
-      '24 Nov 2014 to 30 Nov 2014 = 37m 51s',
-      '17 Nov 2014 to 23 Nov 2014 = 37m 56s',
-      'Total change = -5s'
+      '1 January 2014 to 31 January 2014 = 78.5%',
+      '1 February 2014 to 28 February 2014 = 78.5%',
+      'Total change = 0%'
     ]);
   });
 
   it('should return a textUpdate for a user satisfaction module', function () {
-    var module = this.processed[4];
+    var module = this.processed[2];
     module.textUpdate.should.eql([
-      '15 Dec 2014 to 22 Dec 2014 = 85.6%',
-      '8 Dec 2014 to 14 Dec 2014 = 87.0%',
-      'Total change = -1.4%'
+      '12 January 2015 to 18 January 2015 = 85.8%',
+      '5 January 2015 to 11 January 2015 = 86.2%',
+      'Total change = −0.46%'
     ]);
   });
 });
