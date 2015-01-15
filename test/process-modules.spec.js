9fea00211619385341d447dfa4724358a4c42bb9
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2015-01-15 15:32:19 +0000

    Grouped time series support for summary email including missing data scenarios.

--- test/process-modules.spec.js
+++ test/process-modules.spec.js
@@ -1,52 +1,158 @@
 describe('Process summary modules', function () {
-  var processModules = require('../lib/process-modules.js');
+  var _ = require('underscore'),
+    processModules = require('../lib/process-modules.js'),
+    modules = [];
+  modules.push(require('../node_modules/performanceplatform-client.js' +
+  '/test/fixtures/module-config-kpi.json'));
+  modules.push(require('../node_modules/performanceplatform-client.js' +
+  '/test/fixtures/module-config-single-time-series.json'));
+  modules.push(require('../node_modules/performanceplatform-client.js' +
+  '/test/fixtures/module-config-user-satisfaction-graph.json'));
+  modules.push(require('../node_modules/performanceplatform-client.js' +
+  '/test/fixtures/module-config-grouped-time-series.json'));
 
-  beforeEach(function () {
-    this.dashboardConfig = require('../node_modules/performanceplatform-client.js' +
-    '/test/fixtures/dashboard-response.json');
-    this.dashboardConfig.modules = [];
-    this.dashboardConfig.modules.push(require('../node_modules/performanceplatform-client.js' +
-    '/test/fixtures/module-config-kpi.json'));
-    this.dashboardConfig.modules.push(require('../node_modules/performanceplatform-client.js' +
-    '/test/fixtures/module-config-single-time-series.json'));
-    this.dashboardConfig.modules.push(require('../node_modules/performanceplatform-client.js' +
-    '/test/fixtures/module-config-user-satisfaction-graph.json'));
-    this.processed = processModules(this.dashboardConfig.modules);
+  describe('Complete data', function () {
+
+    beforeEach(function () {
+      this.modules = JSON.parse(JSON.stringify(modules));
+      this.processed = processModules(this.modules);
+    });
+
+    it('returns an array of modules', function () {
+      this.processed.length.should.equal(4);
+    });
+
+    it('should return modules with title property', function () {
+      var module = this.processed[0];
+      module.moduleConfig.title.should.equal('Transactions per year');
+    });
+
+    it('should return a textUpdate for a KPI module', function () {
+      var module = this.processed[0];
+      module.textUpdate.should.eql([
+        '1 July 2013 to 30 June 2014 = 45.8m',
+        '1 April 2013 to 31 March 2014 = 46m',
+        'Total change = −0.27%'
+      ]);
+    });
+
+    it('should return a textUpdate for a single time series module', function () {
+      var module = this.processed[1];
+      module.textUpdate.should.eql([
+        '1 November 2014 to 30 November 2014 = 82.9%',
+        '1 October 2014 to 31 October 2014 = 74.3%',
+        'Total change = +11.57%'
+      ]);
+    });
+
+    it('should return a textUpdate for a user satisfaction module', function () {
+      var module = this.processed[2];
+      module.textUpdate.should.eql([
+        '12 January 2015 to 18 January 2015 = 85.8%',
+        '5 January 2015 to 11 January 2015 = 86.2%',
+        'Total change = −0.46%'
+      ]);
+    });
+
+    it('should return a textUpdate for a grouped time series module', function () {
+      var module = this.processed[3];
+      module.textUpdate.should.eql([
+
+        'Totals',
+        'December 2014 = 3705269',
+        'November 2014 = 3158941',
+        'Total change = 17.3%',
+        '',
+        'DVLA centre',
+        'December 2014 = 13033 (0.4% of total)',
+        'November 2014 = 13720 (0.4% of total)',
+        'Total change = -5.0%',
+        '',
+        'Post Office',
+        'December 2014 = 1062692 (28.7% of total)',
+        'November 2014 = 988061 (31.3% of total)',
+        'Total change = 7.6%',
+        '',
+        'Digital and automated phone',
+        'December 2014 = 2629544 (71.0% of total)',
+        'November 2014 = 2157160 (68.3% of total)',
+        'Total change = 21.9%',
+        ''
+      ]);
+    });
+
   });
 
-  it('returns an array of modules', function () {
-    this.processed.length.should.equal(3);
+  describe('Incomplete data', function () {
+
+    beforeEach(function () {
+      _.each(_.last(modules[3].dataSource.data, 2), function (item) {
+        item['volume:sum'] = null;
+      });
+      this.modules = JSON.parse(JSON.stringify(modules));
+      this.processed = processModules(this.modules);
+    });
+
+    it('should not show percentages for a data point when its missing for grouped time series',
+      function () {
+        var module = this.processed[3];
+        module.textUpdate.should.eql([
+
+          'Totals',
+          'December 2014 = 13033 (data is missing)',
+          'November 2014 = 3158941',
+          'Total change = -99.6%',
+          '',
+          'DVLA centre',
+          'December 2014 = 13033 (100.0% of total)',
+          'November 2014 = 13720 (0.4% of total)',
+          'Total change = -5.0%',
+          '',
+          'Post Office',
+          'December 2014 = no data',
+          'November 2014 = 988061 (31.3% of total)',
+          '',
+          'Digital and automated phone',
+          'December 2014 = no data',
+          'November 2014 = 2157160 (68.3% of total)',
+          ''
+        ]);
+      });
   });
 
-  it('should return modules with title property', function () {
-    var module = this.processed[0];
-    module.moduleConfig.title.should.equal('test');
-  });
+  describe('All data missing', function () {
 
-  it('should return a textUpdate for a KPI module', function () {
-    var module = this.processed[0];
-    module.textUpdate.should.eql([
-      '1 July 2013 to 30 June 2014 = 1',
-      '1 April 2013 to 30 June 2014 = 2',
-      'Total change = −50.00%'
-    ]);
-  });
+    beforeEach(function () {
+      _.each(_.last(modules[3].dataSource.data, 6), function (item) {
+        item['volume:sum'] = null;
+      });
+      this.modules = JSON.parse(JSON.stringify(modules));
+      this.processed = processModules(this.modules);
+    });
 
-  it('should return a textUpdate for a single time series module', function () {
-    var module = this.processed[1];
-    module.textUpdate.should.eql([
-      '1 January 2014 to 31 January 2014 = 78.5%',
-      '1 February 2014 to 28 February 2014 = 78.5%',
-      'Total change = 0%'
-    ]);
-  });
+    it('should not show the totals "data is missing" message when all data is missing' +
+      'for grouped time series',
+      function () {
+        var module = this.processed[3];
+        module.textUpdate.should.eql([
 
-  it('should return a textUpdate for a user satisfaction module', function () {
-    var module = this.processed[2];
-    module.textUpdate.should.eql([
-      '12 January 2015 to 18 January 2015 = 85.8%',
-      '5 January 2015 to 11 January 2015 = 86.2%',
-      'Total change = −0.46%'
-    ]);
+          'Totals',
+          'December 2014 = no data',
+          'November 2014 = no data',
+          '',
+          'DVLA centre',
+          'December 2014 = no data',
+          'November 2014 = no data',
+          '',
+          'Post Office',
+          'December 2014 = no data',
+          'November 2014 = no data',
+          '',
+          'Digital and automated phone',
+          'December 2014 = no data',
+          'November 2014 = no data',
+          ''
+        ]);
+      });
   });
 });
